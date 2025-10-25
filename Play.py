3b11a4220451b40d2dbb38698ef4322d4d58c149
huwhuwhuwhuw <140722567+huwhuwhuwhuw@users.noreply.github.com>
2025-10-25 14:33:48 +0100

    Update Play.py

--- Play.py
+++ Play.py
@@ -6,9 +6,6 @@ Created on Tue Oct 21 15:07:22 2025
 @author: huwtebbutt
 """
 
-    
-
-    
 def Make_Move(Board):
     #Get coordinates of move
     #Get piece coords
@@ -19,7 +16,6 @@ def Make_Move(Board):
         if match:
             x = int(match.group(1))-1
             y = int(match.group(2))-1
-            
             return x,y
         else: return None
 
@@ -38,15 +34,29 @@ def Make_Move(Board):
     def check_Piece_Coords(Board,x,y):
         Piece=Board.get_Piece(x,y)
         if Piece=='Empty':
+            print("Empty Square, try again")
             return False
         else:
-            return True
+            Confirmation = False
+            from re import findall
+            while not Confirmation:
+                Confirmation= input("Correct Piece? y/n\n")
+                Confirmation = ''.join(findall(r'[a-zA-Z]', Confirmation))
+                if Confirmation:
+                    if Confirmation.upper()[0]=='Y':
+                        return True
+                    else:
+                        print("Selected wrong square")
+                        return False
+                else:
+                    Confirmation =False
         
     #Check attempted move is legal
     def check_Move(Board,Piece_Coords,Target_Coords):
         Piece=Board.Board[Piece_Coords[0],Piece_Coords[1]]
         #Check move complies with piece movement rules
         if not Piece.is_legal_move(Board,Target_Coords[0],Target_Coords[1]):
+            print('Illegal Move')
             return False
         
         #Check move complies with board constraints
@@ -54,6 +64,7 @@ def Make_Move(Board):
         Relative_x=Piece_Coords[0]-Target_Coords[0]
         Relative_y=Piece_Coords[1]-Target_Coords[1]
         if not Board.check_Board_Constraints(Piece,Relative_x,Relative_y):
+            print('Board constraints')
             return False
         
         #Update board to see if King is in check
@@ -63,12 +74,23 @@ def Make_Move(Board):
         if Board.is_Player_in_Check():
             #Player is in check at the end of their turn, illegal, revert board state
             Board.Board=Old_Board
+            Piece=Board.get_Piece(Piece_Coords[0],Piece_Coords[1])
+            if Piece.Castling:
+                Piece.Castling=False
+            elif Piece.Enpassanting:
+                Piece.Enpassanting=False
+            print('Check issue')
             return False
-        
         return True
     
     
-    
+    if Board.is_Checkmate():
+        if Board.Turn=="White":
+            Winner="Black"
+        elif Board.Turn=="Black":
+            Winner="White"
+        
+        print(f'Checkmate! Winner={Winner}')
     Board.show_board()
     print(f"Turn = {Board.Turn}")
     if Board.Check:
@@ -83,7 +105,8 @@ def Make_Move(Board):
         
         Piece_Coords=get_Piece_Coords()
         if Piece_Coords:
-            print(f"Piece= {Board.get_Piece(Piece_Coords[0],Piece_Coords[1]).Name}")
+            Piece=Board.get_Piece(Piece_Coords[0],Piece_Coords[1])
+            print(f"Piece= {Piece.Colour} {Piece.Name}")
             PX=Piece_Coords[0]
             PY=Piece_Coords[1]
             if check_Piece_Coords(Board,PX,PY):
@@ -93,12 +116,14 @@ def Make_Move(Board):
                     if is_Move_Legal:
                         Board.Switch_Turn()
                         Completed_Move=True
+                        print("Move successfull")
+                        print("*"*60)
                     else:
                         print('Illegal Move, try again')
                 else:
                     print('Incorrect Format, try again')
             else:
-                print('Incorrect Square, try again')
+                pass
         else:
             print('Incorrect Format, try again')
     
@@ -109,9 +134,3 @@ if __name__ == "__main__":
     Board=Board(x,y)
     Make_Move(Board)
     Board.show_board()
-        
-    
-        
-    
-    
-    
